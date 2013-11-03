2958b36d1046c0d28de7bc1202c572f9d51c1b17
Eva <spointense@yahoo.com>
2013-11-03 13:16:20 +0200

    added comments for class constructors

--- src/art.java
+++ src/art.java
@@ -6,15 +6,15 @@
  * To change this template use File | Settings | File Templates.
  */
 class Art {
-    Art() { System.out.println("Art constructor"); }
+    Art() { System.out.println("Art constructor"); }  // Constructor of the art class, will be called 1
 }
 
 class Drawing extends Art {
-    Drawing() { System.out.println("Drawing constructor"); }
+    Drawing() { System.out.println("Drawing constructor"); } // Constructor of the Drawing class which extends the Art class, will be called 2
 }
 
 class Cartoon extends Drawing {
-    public Cartoon() { System.out.println("Cartoon constructor"); }
+    public Cartoon() { System.out.println("Cartoon constructor"); } // Constructor of the cartoon class which extends the Drawing class, will be called 3
     public static void main(String[] args) {
         Cartoon x = new Cartoon();
     }
